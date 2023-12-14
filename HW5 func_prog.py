21f0ccbc44d97f63d0e88170b5f3d5c7b5072f03
AlexeyChutko <147832971+AlexeyChutko@users.noreply.github.com>
2023-12-14 15:23:49 +0300

    Add files via upload

---
+++ HW5 func_prog.py
@@ -0,0 +1,25 @@
+def create_counter(alphabet):
+    def count_occurrences(sequence):
+        # Генерация словаря для хранения статистики
+        # Словарь, в котором ключи - символы из алфавита, а значения - количество вхождений каждого символа в последовательность.
+        stats = {char: 0 for char in alphabet}
+
+        # Подсчет встречаемости символов в последовательности
+        for char in sequence:
+            if char in stats:
+                stats[char] += 1
+
+        return stats
+
+    return count_occurrences
+
+# Пример использования функции
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+count_func = create_counter(alphabet)
+
+sequence = input()
+statistics = count_func(sequence)
+
+# Вывод статистики
+for char, count in statistics.items():
+    print(f"Symbol: {char}, Count: {count}")
